e0780b7f4ece93db411128ca6fbfba70bffd1a69
sarovin <sarovin86@gmail.com>
2016-06-01 14:18:13 +0200

    FIX __DEV__ is not defined

--- test/index.test.js
+++ test/index.test.js
@@ -4,26 +4,33 @@ import { View } from 'react-native';
 
 import test from 'ava';
 import { Select, Picker } from '../';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
+
+let prefix = '';
+
+const Test = React.createClass({
+
+  render() {
+    return (
+      <View>
+        <Select
+          ref={"SELET1"}
+          pickerRef={() => this.refs.picker}
+          selected={prefix}
+        />
+        <Picker
+          ref={'picker'}
+          selectedValue={'IT'}
+          onSubmit={(option) => {
+            prefix = option;
+          }}
+        />
+      </View>
+    )
+  }
+});
 
 test(t => {
-  let prefix = '';
-  const wrapper = render(
-    <View>
-      <Select
-        ref={"SELET1"}
-        pickerRef={() => this.refs.picker}
-        selected={prefix}
-      />
-      <Picker
-        ref={'picker'}
-        selectedValue={'IT'}
-        onSubmit={(option) => {
-          prefix = option;
-        }}
-      />
-    </View>
-  );
-
+  const wrapper = shallow(<Test/>);
   t.truthy(wrapper);
 });
